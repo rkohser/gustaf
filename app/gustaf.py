479b12311319c22e32a0dcf7f053e841968cf402
rkohser <rkohser@gmail.com>
2015-11-24 22:13:43 +0100

    Project setup with bower. Angular to retrieve shows and episodes. Removed unneeded files.

--- app/gustaf.py
+++ app/gustaf.py
@@ -21,20 +21,18 @@ if __name__ == '__main__':
     class Application(tornado.web.Application):
 
         def __init__(self):
-            root = os.path.dirname(__file__)
+            root = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
             handlers = [
-                (r"/static/(.*)", tornado.web.StaticFileHandler, {"path": "static"}),
                 (r"/show", ShowHandler, {'name': 'show'}),
                 (r"/play", PlayHandler, {'name': 'play'}),
                 (r"/shows", ShowListHandler),
                 (r"/episodes/([^/]*)", EpisodeHandler),
-                (r"/(.*)", tornado.web.StaticFileHandler, {"path": root, "default_filename": "index.html"})
+                (r"/(.*)", tornado.web.StaticFileHandler, {"path": root, "default_filename": "app/index.html"})
             ]
 
             settings = {
                 'debug': True,
                 'template_path': os.path.join(os.path.dirname(__file__), 'templates'),
-                'static_path': os.path.join(os.path.dirname(__file__), 'static')
             }
             tornado.web.Application.__init__(self, handlers, **settings)
 
