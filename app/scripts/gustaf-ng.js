40d8eb216118be07ef7cc223ec0d65ff15070512
rkohser <rkohser@gmail.com>
2015-11-24 22:20:18 +0100

    Added missing scripts. Updated gitignore

---
+++ app/scripts/gustaf-ng.js
@@ -0,0 +1,21 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+var app = angular.module('gustafApp', []);
+app.controller('gustafController', function($scope, $http) {
+    $http.get("/shows").success(
+        function(response) {
+            $scope.shows = response;
+        }
+    );
+    
+    $scope.getEpisodesPerShowId = function(showId) {
+        $http.get("/episodes/" + showId).success(
+            function(response) {
+                $scope.episodes = response;
+            });
+    };
+});
